cc4574ff0925f2b75a6f1a155e78a8374de35a73
Alan Rodriguez <alan@localhost.localdomain>
2018-10-15 17:02:58 -0300

    creating a basic frontend for our application

---
+++ desapp-groupG-frontend/src/main/java/ar/edu/unq/desapp/grupoG/DesappGroupGFrontendApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesappGroupGFrontendApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesappGroupGFrontendApplication.class, args);
+	}
+}
