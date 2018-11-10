d38756bc6a8cf37eb4e9b25756cb3402fe24dc57
alannrod <alan.nicolas.rodriguez@gmail.com>
2018-11-10 20:04:32 -0300

    probando otro ejemplo de i18n

--- desapp-groupG-frontend/src/main/java/ar/edu/unq/desapp/grupoG/AppConfig.java
+++ desapp-groupG-frontend/src/main/java/ar/edu/unq/desapp/grupoG/AppConfig.java
@@ -1,32 +1,36 @@
 package ar.edu.unq.desapp.grupoG;
 
-
 import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-public class Internationalization  extends WebMvcConfigurationSupport {
+@Configuration
+public class AppConfig extends WebMvcConfigurerAdapter {
 	
-	 @Bean
+	@Bean
 	   public LocaleResolver localeResolver() {
-	      SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
-	      sessionLocaleResolver.setDefaultLocale(Locale.US);
-	      return sessionLocaleResolver;
+	       SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+	       sessionLocaleResolver.setDefaultLocale(Locale.US);
+	       return sessionLocaleResolver;
 	   }
+	 
 	   @Bean
 	   public LocaleChangeInterceptor localeChangeInterceptor() {
-	      LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-	      localeChangeInterceptor.setParamName("language");
-	      return localeChangeInterceptor;
+	       LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+	       lci.setParamName("lang");
+	       return lci;
 	   }
+	 
 	   @Override
 	   public void addInterceptors(InterceptorRegistry registry) {
-	      registry.addInterceptor(localeChangeInterceptor());
+	       registry.addInterceptor(localeChangeInterceptor());
 	   }
+
 
 }
