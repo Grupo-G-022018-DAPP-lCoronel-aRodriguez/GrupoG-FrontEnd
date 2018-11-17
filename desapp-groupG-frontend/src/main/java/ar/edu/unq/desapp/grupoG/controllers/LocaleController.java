b50648f523e0618d97798c1940c0096fb058039c
Alan Rodriguez <alan.nicolas.rodriguez@gmai.com>
2018-11-17 12:09:07 -0300

    moldeando una pagina como la gente

--- desapp-groupG-frontend/src/main/java/ar/edu/unq/desapp/grupoG/controllers/LocaleController.java
+++ desapp-groupG-frontend/src/main/java/ar/edu/unq/desapp/grupoG/controllers/LocaleController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class LocaleController {
 	@RequestMapping(value = "/locale", method = RequestMethod.GET)
 	   public String getLocalePage() {
-	       return "my-locale";
+	       return "index";
 	   }
 }
