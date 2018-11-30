77e8e77139e895a1bfb3e7fb11d7fe95823cc030
Alan Rodriguez <alan.nicolas.rodriguez@gmai.com>
2018-11-30 08:45:52 -0300

    solucionado el login de springboot inicial

--- desapp-groupG-frontend/src/main/java/ar/edu/unq/desapp/grupoG/controllers/HomeController.java
+++ desapp-groupG-frontend/src/main/java/ar/edu/unq/desapp/grupoG/controllers/HomeController.java
@@ -1,11 +1,11 @@
 package ar.edu.unq.desapp.grupoG.controllers;
-import org.springframework.security.access.annotation.Secured;
+//import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
