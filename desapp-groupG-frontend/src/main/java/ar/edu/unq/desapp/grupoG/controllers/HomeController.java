5c1d24e9fc148cc245da2d97b0e9d09f54aa78f6
Alan Rodriguez <alan.nicolas.rodriguez@gmai.com>
2018-11-30 10:48:14 -0300

    sigue sin reconocer el login

--- desapp-groupG-frontend/src/main/java/ar/edu/unq/desapp/grupoG/controllers/HomeController.java
+++ desapp-groupG-frontend/src/main/java/ar/edu/unq/desapp/grupoG/controllers/HomeController.java
@@ -6,12 +6,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.bind.annotation.ResponseBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * @author dgomez
+ */
 @Controller
 public class HomeController {
 
+    private final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @Secured(value="IS_AUTHENTICATED_FULLY")
     @RequestMapping(value="/userHome", method = RequestMethod.GET)
-    public void getUserProfile(Model model) {
-        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+    public @ResponseBody Object getUserProfile() {
+
+        logger.info("Requested user profile");
+        logger.info("Beans :" + SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        return SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
+
 }
