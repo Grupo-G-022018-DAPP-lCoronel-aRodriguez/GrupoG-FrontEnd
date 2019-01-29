af79192907c2b47f2822ac711ca14559ad2b7fb9
Alan Rodriguez <alan.nicolas.rodriguez@gmai.com>
2019-01-29 12:48:11 -0300

    login con google

--- desapp-groupG-frontend/src/main/java/ar/edu/unq/desapp/grupoG/DesappGroupGFrontendApplication.java
+++ desapp-groupG-frontend/src/main/java/ar/edu/unq/desapp/grupoG/DesappGroupGFrontendApplication.java
@@ -2,8 +2,12 @@ package ar.edu.unq.desapp.grupoG;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
+//para poder usar el login de google debemos usar esta anotacion
+
 public class DesappGroupGFrontendApplication {
 
 
